2e5099f3784d304303a413dd63a80d4c6012b861
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-08-05 18:52:43 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,11 @@
-# code here!
+class School
+attr_reader :roster 
+def initialize(name)
+  @school = name
+  @roster = {}
+end
+def add_student(name, grade)
+  roster[grade] || []
+  roster[grade] << name
+end
+end
